566803b16e5d7afea84187dca7448e3caacc31ea
karbunkul <karbunkul@yourtask.ru>
2013-05-08 13:06:58 +0600

    Настройки

--- plusoshare.tpl.php
+++ plusoshare.tpl.php
@@ -1,10 +1,3 @@
-<script type="text/javascript">(function() {
- if (window.pluso)if (typeof window.pluso.start == "function") return;
- var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
- s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
- s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://share.pluso.ru/pluso-like.js';
- var h=d[g]('head')[0] || d[g]('body')[0];
- h.appendChild(s);
-})();</script>
+<?php print $script;?>
 
 <center><div class="pluso" <?php print $plusoshare;?>></div></center>
